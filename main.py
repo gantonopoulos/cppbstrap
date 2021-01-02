a11a4131d89fc0687fa95513205eb7b7fcc68457
Georgios Antonopoulos <georanto@gmail.com>
2021-01-02 12:28:49 +0100

    Adds a check of the project workspace target path

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sys, argparse
+import os.path
+import argparse
+from typing import Callable
+
 
 def create_project_workspace():
     pass
@@ -16,13 +19,23 @@ def create_cmake_lists():
     pass
 
 
+def validate_path(path_to_validate: str):
+    if not os.path.isdir(path_to_validate):
+        raise argparse.ArgumentError('Invalid path!')
+    if not os.access(path_to_validate, os.W_OK):
+        raise argparse.ArgumentError('Target directory cannot be written. Check access rights!')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='cppbstrap')
-    parser.add_argument('name', help='The name of the project')
+    parser.add_argument('-p', '--Path', default='.', help='The target directory in which the project\'s workspace '
+                                                          'directory will be placed')
+    parser.add_argument('Name', help='The name of the project')
     args = parser.parse_args()
-    print(args.name)
-    #parser.print_help()
-    #args = parser.parse_args()
+    print('Name:' + args.Name)
+    print('Path :' + os.path.abspath(args.Path))
+    validate_path(os.path.abspath(args.Path))
+
 
 
     # create_project_workspace()
